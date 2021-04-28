a82109242cb8679219f90d01d6b52d9332d7dea0
Piotr Kozakowski <p.kozakowski@mimuw.edu.pl>
2021-04-28 02:15:55 +0200

    Move most instance implementations out of Labels.

--- src/Data/Record/Hom.hs
+++ src/Data/Record/Hom.hs
@@ -3,9 +3,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -24,6 +22,8 @@ module Data.Record.Hom
     , (!)
     , get
     , set
+    , getIn
+    , setIn
     , toList
     , deriveUnary
     , deriveAbsolute
@@ -35,10 +35,8 @@ module Data.Record.Hom
 import Data.Coerce
 import Data.Constraint
 import qualified Data.Deriving as Deriving
-import Data.Kind hiding (Type)
 import Data.Monoid
 import Data.Proxy
-import Data.Type.Equality
 import GHC.OverloadedLabels
 import GHC.TypeLits
 import Language.Haskell.TH
@@ -63,7 +61,7 @@ data HomRec ls t where
 infix 6 :=
 infixr 5 :&
 
-instance l ~ l' => IsLabel (l :: Symbol) (Proxy l') where
+instance l ~ l' => IsLabel l (Proxy l') where
     fromLabel = Proxy
 
 -- | Witness types needed to avoid overlapping instances of Has.
@@ -81,12 +79,10 @@ class wtn ~ Where l ls => HasAt l ls wtn where
     set :: Proxy l -> t -> HomRec ls t -> HomRec ls t
 
 instance HasAt l (l ': ls) AtHead where
-
     get _ (_ := x :& _) = x
     set lp x (_ :& r) = lp := x :& r
 
 instance (HasAt l ls i, Where l (l' ': ls) ~ AtTail) => HasAt l (l' ': ls) AtTail where
-
     get lp (_ :& r) = get lp r
     set lp x (fld :& r) = fld :& set lp x r
 
@@ -94,65 +90,61 @@ instance (HasAt l ls i, Where l (l' ': ls) ~ AtTail) => HasAt l (l' ': ls) AtTai
 membershipMonotonicity :: Has l ls :- Has l (l' ': ls)
 membershipMonotonicity = unmapDict unsafeCoerce
 
-type family NoDuplicateIn (l :: u) (ls :: [u]) :: Constraint where
-    NoDuplicateIn l '[] = ()
-    NoDuplicateIn l (l ': ls) = TypeError (Text "Duplicate key " :<>: ShowType l :<>: Text ".")
-    NoDuplicateIn l (_ ': ls) = NoDuplicateIn l ls
-
--- | Label with a membership proof.
+-- | Label with a proof of membership.
 data LabelIn ls = forall l. LabelIn (Dict (Has l ls))
+
+getIn :: LabelIn ls -> HomRec ls t -> t
+getIn (LabelIn (Dict :: Dict (Has l ls))) = get (Proxy :: Proxy l)
+
+setIn :: LabelIn ls -> t -> HomRec ls t -> HomRec ls t
+setIn (LabelIn (Dict :: Dict (Has l ls))) = set (Proxy :: Proxy l)
+
+toList :: HomRec ls t -> [(LabelIn ls, t)]
+toList Empty = []
+toList (lp := x :& r)
+    = (LabelIn (Dict :: Dict (HasAt l (l ': ls) AtHead)), x) : fmap inj (toList r) where
+        inj :: (LabelIn ls, t) -> (LabelIn (l ': ls), t)
+        inj (LabelIn proof, x) = (LabelIn proof', x) where
+            proof' = mapDict membershipMonotonicity proof
 
 (!) :: Has l ls => HomRec ls t -> Proxy l -> t
 (!) = flip get
 
 infixl 9 !
 
+type family NoDuplicateIn (l :: u) (ls :: [u]) :: Constraint where
+    NoDuplicateIn l '[] = ()
+    NoDuplicateIn l (l ': ls) = TypeError (Text "Duplicate key " :<>: ShowType l :<>: Text ".")
+    NoDuplicateIn l (_ ': ls) = NoDuplicateIn l ls
+
 class Labels ls where
-    fmapImpl :: (a -> b) -> HomRec ls a -> HomRec ls b
-    pureImpl :: a -> HomRec ls a
-    apImpl :: HomRec ls (a -> b) -> HomRec ls a -> HomRec ls b
-    foldMapImpl :: Monoid m => (a -> m) -> HomRec ls a -> m
-    sequenceAImpl :: Applicative f => HomRec ls (f a) -> f (HomRec ls a)
+    fill :: t -> HomRec ls t
     symbolVals :: Proxy ls -> [String]
-    toList :: HomRec ls t -> [(LabelIn ls, t)]
 
 instance Labels '[] where
-    fmapImpl _ _ = Empty
-    pureImpl _ = Empty
-    apImpl _ _ = Empty
-    foldMapImpl _ _ = mempty
-    sequenceAImpl _ = pure Empty
+    fill _ = Empty
     symbolVals _ = []
-    toList _ = []
 
 instance (NoDuplicateIn l ls, KnownSymbol l, Labels ls) => Labels (l ': ls) where
-    fmapImpl f (lp := x :& r) = lp := f x :& fmapImpl f r
-    pureImpl (x :: t) = Proxy := x :& (pureImpl x :: HomRec ls t)
-    apImpl (lp := f :& fr) (_ := x :& xr) = lp := f x :& fr `apImpl` xr
-    foldMapImpl f (lp := x :& r) = f x <> foldMapImpl f r
-    sequenceAImpl (lp := x :& r) = (:&) . (lp :=) <$> x <*> sequenceAImpl r
+    fill x = Proxy := x :& fill x
     symbolVals _ = symbolVal (Proxy :: Proxy l) : symbolVals (Proxy :: Proxy ls)
 
-    toList (lp := x :& r)
-        = (LabelIn (Dict :: Dict (HasAt l (l ': ls) AtHead)), x)
-        : fmap inj (toList r)
-        where
-            inj :: (LabelIn ls, t) -> (LabelIn (l ': ls), t)
-            inj (LabelIn (proof :: Dict (Has l' ls)), x) = (LabelIn proof', x) where
-                proof' = mapDict membershipMonotonicity proof
-
 instance Labels ls => Functor (HomRec ls) where
-    fmap = fmapImpl
+    fmap _ Empty = Empty
+    fmap f (lp := x :& r) = lp := f x :& fmap f r
 
 instance Labels ls => Applicative (HomRec ls) where
-    pure = pureImpl
-    (<*>) = apImpl
+    pure = fill
+    Empty <*> Empty = Empty
+    (lp := f :& fr) <*> (_ := x :& xr) = lp := f x :& (fr <*> xr)
 
 instance Labels ls => Foldable (HomRec ls) where
-    foldMap = foldMapImpl
+    foldMap _ Empty = mempty
+    foldMap f (lp := x :& r) = f x <> foldMap f r
 
 instance Labels ls => Traversable (HomRec ls) where
-    sequenceA = sequenceAImpl
+    sequenceA Empty = pure Empty
+    sequenceA (lp := x :& r) = (:&) . (lp :=) <$> x <*> sequenceA r
 
 instance (Labels ls, Show t) => Show (HomRec ls t) where
     show = show . zip (symbolVals (Proxy :: Proxy ls)) . map snd . toList
@@ -170,7 +162,7 @@ instance (Labels ls, RightModule a t) => RightModule a (HomRec ls t) where
     p *. n = (*. n) <$> p
 
 instance (Labels ls, Monoidal t) => Monoidal (HomRec ls t) where
-    zero = pureImpl zero
+    zero = pure zero
 
 instance (Labels ls, Module a t) => Module a (HomRec ls t)
 
@@ -202,10 +194,10 @@ deriveDelta scr abs rel = do
         relP v = conP relCons [varP v]
     [d| instance Labels ls => Delta $(conT scr) ($(conT abs) ls) ($(conT rel) ls) where
 
-            delta $(absP xn) $(absP yn) = $(conE relCons) $ (delta `fmapImpl` x) `apImpl` y
+            delta $(absP xn) $(absP yn) = $(conE relCons) $ delta <$> x <*> y
 
             sigma $(absP xn) $(relP yn)
-                = $(conE absCons) <$> (sequenceAImpl $ sigma `fmapImpl` x `apImpl` y) |]
+                = $(conE absCons) <$> (sequenceA $ sigma <$> x <*> y) |]
     where
         xn = mkName "x"
         yn = mkName "y"
@@ -219,9 +211,9 @@ deriveKappa a b c = do
         bP v = conP bCons [varP v]
         cP v = conP cCons [varP v]
     [d| instance Labels ls => Kappa ($(conT a) ls) ($(conT b) ls) ($(conT c) ls) where
-            kappa $(aP xn) $(bP yn) = $(conE cCons) $ (kappa `fmapImpl` x) `apImpl` y
-            kappa' $(aP xn) $(cP yn) = $(conE bCons) $ (kappa' `fmapImpl` x) `apImpl` y
-            pi $(bP xn) $(cP yn) = $(conE aCons) $ (pi `fmapImpl` x) `apImpl` y |]
+            kappa $(aP xn) $(bP yn) = $(conE cCons) $ kappa <$> x <*> y
+            kappa' $(aP xn) $(cP yn) = $(conE bCons) $ kappa' <$> x <*> y
+            pi $(bP xn) $(cP yn) = $(conE aCons) $ pi <$> x <*> y |]
     where
         xn = mkName "x"
         yn = mkName "y"
