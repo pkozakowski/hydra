55f59734ff1a3619cd0b813bf006b703905fe1c6
Piotr Kozakowski <p.kozakowski@mimuw.edu.pl>
2021-06-27 00:50:49 +0200

    Remove empty newlines from comments in Market.Types.

--- src/Market/Types.hs
+++ src/Market/Types.hs
@@ -37,70 +37,58 @@ instance (Semiring a, Additive a) => RightModule a a where
 instance (Semiring a, Additive a) => Module a a
 
 -- | Underlying scalar type.
---
 type Scalar = Fraction Integer
 
 -- | Amount of a given Asset.
---
 newtype Amount = Amount Scalar
 newtype AmountDelta = AmountDelta Scalar
 
 -- | Portfolio is a record of Amounts for each Asset.
---
 newtype Portfolio assets = Portfolio (HomRec assets Amount)
 newtype PortfolioDelta assets = PortfolioDelta (HomRec assets AmountDelta)
 
 deriveQuantityInstances ''Scalar ''Amount ''AmountDelta ''Portfolio ''PortfolioDelta
 
 -- | Price of a given Asset, measured in units of the baseline asset (e.g. USD).
---
 newtype Price = Price Scalar
 newtype PriceDelta = PriceDelta Scalar
 
 -- | Record of Prices for each Asset.
---
 newtype Prices assets = Prices (HomRec assets Price)
 newtype PriceDeltas assets = PriceDeltas (HomRec assets PriceDelta)
 
 deriveQuantityInstances ''Scalar ''Price ''PriceDelta ''Prices ''PriceDeltas
 
 -- | Possessed Value, measured in units of the baseline asset (e.g. USD).
---
 newtype Value = Value Scalar
 newtype ValueDelta = ValueDelta Scalar
 
 -- | Value held in each Asset.
---
 newtype Values assets = Values (HomRec assets Value)
 newtype ValueDeltas assets = ValueDeltas (HomRec assets ValueDelta)
 
 deriveQuantityInstances ''Scalar ''Value ''ValueDelta ''Values ''ValueDeltas
 
 -- | Value = Amount * Price.
---
 deriveKappaDivision ''Scalar ''Value ''Amount ''Price
 deriveKappaDivision ''Scalar ''ValueDelta ''AmountDelta ''PriceDelta
 HR.deriveKappa ''Values ''Portfolio ''Prices
 HR.deriveKappa ''ValueDeltas ''PortfolioDelta ''PriceDeltas
 
 -- | Share of some quantity in a bigger whole. Doesn't make sense on its own.
---
 newtype Share = Share Scalar
 newtype ShareDelta = ShareDelta Scalar
 
 -- | Distribution on Assets. Sums to 1.
---
 newtype Distribution assets = Distribution (HomRec assets Share)
 
 -- | Delta between two Distributions. Sums to 0.
---
 newtype DistributionDelta assets = DistributionDelta (HomRec assets ShareDelta)
 
 deriveDistributionInstances ''Scalar ''Share ''ShareDelta ''Distribution ''DistributionDelta
 
 -- | Only Values are (Un)Normalizable, because it only makes sense to add up Values held in
 -- different Assets.
---
 deriveUnnormalizable ''Scalar ''Distribution ''Value ''Values
 deriveUnnormalizable ''Scalar ''DistributionDelta ''ValueDelta ''ValueDeltas
 deriveNormalizable ''Scalar '' Distribution ''Value ''Values
